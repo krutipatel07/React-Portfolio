3e95134c9ff596b9a0cebbd87f0f077976f56365
Kruti Patel <kruti.patel0709@gmail.com>
2021-09-03 23:56:55 -0400

    add an experience into the resume

--- src/lib/resume.js
+++ src/lib/resume.js
@@ -1,48 +1,25 @@
 const experience = [
     {
         id: 1,
-        title: 'Forensic Firearms Specialist',
-        company: 'Orange County Sheriff\'s Office',
-        dates: 'November 2017 to Present',
+        title: 'web developer',
+        company: 'PEaaS',
+        dates: 'April 2018 to Novrmber 2019',
         description: [
-            'Eliminated a backlog of over 1,000 firearms through efficient time and caseload management.',
-            'Prepare technical reports and perform technical reviews of peer reports.',
-            'Maintain open lines of communication with investigators and prosecutors to deliver the best available forensic evidence for trial.'
+            'Managed and co-led 5+ web development projects from numerous customers focused on eCommerce, data management, app development, money-transfer product design and development, and blog posts.',
+            'Integrated various capabilities using APIs and third-party plugins for interactive, user-friendly, and efficient web applications.',
+            'Effectively translated client requirements into application designs and systems requirements.'
         ]
     },
     {
         id: 2,
-        title: 'Crime Scene Investigator II',
-        company: 'Orange County Sheriff\'s Office',
-        dates: 'August 2013 to November 2017',
+        title: 'Freelance Frontend Developer',
+        company: 'My Self',
+        dates: 'January 2020 to April 2021',
         description: [
-            'Utilized critical thinking and analytical skills to process crime scenes.',
-            'Applied logic, reasoning, and problem solving skills to conduct investigations.',
-            'Explained complex scientific and mathematical concepts through over 600 training hours as a certified Field Training Officer.',
-            'Prepared demonstrative tools for courtroom testimony.'
-        ]
-    },
-    {
-        id: 3,
-        title: 'Crime Laboratory Technician II',
-        company: 'Baltimore Police Department',
-        dates: 'June 2011 to August 2013',
-        description: [
-            'Responded to, evaluated, and processed thousands of crime scenes.',
-            'Prepared reports and diagrams for courtroom testimony.'
-        ]
-    },
-    {
-        id: 4,
-        title: 'Academic Resource Center Tutor',
-        company: 'University of Baltimore',
-        dates: 'September 2008 to May 2010',
-        description: [
-            'Tutored peers in algebra, statistics, and criminal justice.',
-            'Organized and led group study sessions.',
-            'Pioneered an online tutoring program that gave students the option to learn remotely.'
+            'Designed and developed website layouts and efficient applications for FinTech (MEDICI Global) and eCommerce (Swych) platforms as a freelance contractor at PEaaS (a Finablr Company).',
         ]
     }
+
 ];
 
 export default experience;
